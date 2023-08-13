9561b945ac79b7d874f8c2479be9943214216bc9
Evgeniy Novikov <termius46@gmail.com>
2023-08-13 19:28:57 +0300

    feat: implement burger menu

--- main.js
+++ main.js
@@ -2,3 +2,32 @@
 // import javascriptLogo from './javascript.svg';
 // import viteLogo from '/vite.svg';
 // import { setupCounter } from './counter.js';
+
+const burgerBtn = document.querySelector('.burger');
+const menu = document.querySelector('.nav');
+const bg = document.querySelector('.bg');
+
+function handleBurgerShow() {
+  burgerBtn.classList.toggle('burger--active');
+  menu.classList.toggle('nav--active');
+  document.body.classList.toggle('lock');
+  bg.classList.toggle('bg--active');
+}
+
+bg.addEventListener('click', () => {
+  handleBurgerShow();
+});
+
+burgerBtn.addEventListener('click', () => {
+  handleBurgerShow();
+});
+
+menu.addEventListener('click', (e) => {
+  if (menu.classList.contains('nav--active')) {
+    if (e.target.classList.contains('menu__link')) {
+      setTimeout(() => {
+        handleBurgerShow();
+      }, 100);
+    }
+  }
+});
